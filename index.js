f4c0a0a280883fa667d29775ce9b6369f29429c9
Austin Arlint <aarlint@gmail.com>
2022-05-19 08:39:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const core = require('@actions/core');
 const dotenvAction = require('./dotenv_action');
 try {
   const dotenvFile = core.getInput('path');
-  const toLower = core.getInput('toLower');
   const logVariables = core.getInput('log-variables').toLowerCase() === 'true';
   const maskVariables =
     core.getInput('mask-variables').toLowerCase() === 'true';
-  const variables = dotenvAction(dotenvFile, logVariables, toLower);
+  const variables = dotenvAction(dotenvFile, logVariables);
 
   if (maskVariables) {
     for (const key in variables) {
